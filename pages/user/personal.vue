0e06bc7291d5218f82341a699cffebaf8a3cbf97
jack <1010330717@qq.com>
2019-07-07 17:54:41 +0800

    攻略初步完成

---
+++ pages/user/personal.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="geren">
+        个人主页暂未开通
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+ .geren{
+     width: 1000px;
+     margin: 100px auto;
+     text-align: center
+ }
+</style>
